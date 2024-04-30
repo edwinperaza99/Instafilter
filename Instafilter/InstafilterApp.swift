0f6d0e4c4b95326172fcd98c14c20bf6355e82ef
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-30 14:57:46 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by csuftitan on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
